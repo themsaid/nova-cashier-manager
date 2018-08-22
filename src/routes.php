6a0e39f20229aec8dc307d2741ef794d5712ab1e
Mohamed Said <themohamedsaid@gmail.com>
2018-08-22 12:45:53 +0200

    wip

--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,5 @@
 <?php
 
-use Carbon\Carbon;
-use Stripe\Stripe;
-
 Route::get('/nova-cashier-tool-api/user/{id}', 'Themsaid\CashierTool\CashierToolController@user');
 Route::post('/nova-cashier-tool-api/user/{id}/cancel', 'Themsaid\CashierTool\CashierToolController@cancelSubscription');
 Route::post('/nova-cashier-tool-api/user/{id}/resume', 'Themsaid\CashierTool\CashierToolController@resumeSubscription');
